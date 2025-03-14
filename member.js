571cb1b85827f6a54fe8f5003250c066d0e6badd
Terry Pham <61438548+RawBoeuf@users.noreply.github.com>
2025-03-14 06:40:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h3>Skills</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+    </div>
+  );
+}
